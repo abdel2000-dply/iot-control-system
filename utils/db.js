12db5f22d81e9cd38fe50288f5a0c59c8b7c816f
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-24 00:39:49 +0100

    using mongoose instead

--- utils/db.js
+++ utils/db.js
@@ -1,35 +1,35 @@
-import e from 'express';
-import { MongoClient } from 'mongodb';
+// import express from 'express';
+// import { MongoClient } from 'mongodb';
 
-class DBClient {
-  constructor() {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'iot_system';
+// class DBClient {
+//   constructor() {
+//     const host = process.env.DB_HOST || 'localhost';
+//     const port = process.env.DB_PORT || 27017;
+//     const database = process.env.DB_DATABASE || 'iot_system';
 
-    const url = `mongodb://${host}:${port}`;
+//     const url = `mongodb://${host}:${port}`;
 
-    this.connected = false;
-    this.client = new MongoClient(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    this.client
-      .connect()
-      .then(() => {
-        this.db = this.client.db(database);
-        this.connected = true;
-      })
-      .catch((error) => {
-        console.log('MongoDB connection error:', error);
-      });
-  }
+//     this.connected = false;
+//     this.client = new MongoClient(url, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     this.client
+//       .connect()
+//       .then(() => {
+//         this.db = this.client.db(database);
+//         this.connected = true;
+//       })
+//       .catch((error) => {
+//         console.log('MongoDB connection error:', error);
+//       });
+//   }
 
-  isAlive() {
-    return this.connected;
-  }
+//   isAlive() {
+//     return this.connected;
+//   }
 
-}
+// }
 
-const dbClient = new DBClient();
-export default dbClient;
+// const dbClient = new DBClient();
+// export default dbClient;
