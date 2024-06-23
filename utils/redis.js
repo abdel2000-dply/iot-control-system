f9d99b408b4aea33c7e480181eec398a57609127
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-23 01:34:19 +0100

    Add Redis client class for caching functionality

---
+++ utils/redis.js
@@ -0,0 +1,42 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient({
+      host: process.env.REDIS_HOST || 'localhost',
+      port: process.env.REDIS_PORT || 6379,
+    });
+    this.isConnected = true;
+    this.client.on('error', (error) => {
+      console.error(error);
+      this.isConnected = false;
+    });
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+
+    this.client.on('connect', () => {
+      this.isConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+  async get(key) {
+    return this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    await this.delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
