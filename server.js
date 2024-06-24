6a2f5972d239616ae8f57f9be552ac839629c064
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-24 01:35:56 +0100

    Connect to MongoDB for database operations
    The code changes in `server.js` connect to MongoDB using Mongoose for performing database operations. This is necessary to store and retrieve user data in the application.

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 import express from 'express';
-import routes from './routes/index';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import routes from './routes/auth';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/', routes);
+app.use('/api/auth/', routes);
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((error) => console.log('MongoDB connection error:', error));
 
 app.listen(PORT, () => {
   console.log('Server is running on port', PORT);
