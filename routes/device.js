495c0fea8f636ba254c26b9a3b01fee2e402d8d5
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-26 22:29:11 +0100

    Update Device Routes

--- routes/device.js
+++ routes/device.js
@@ -6,12 +6,16 @@ const deviceRoutes = express.Router();
 
 // POST - Add a new device
 deviceRoutes.post('/', auth, DeviceController.newDevice);
+
 // GET - Get a device by ID
 deviceRoutes.get('/:deviceId', auth, DeviceController.getDeviceById);
+
 // GET - List all devices of that user
 deviceRoutes.get('', auth, DeviceController.listAllDevices);
+
 // GET - Get status of a device by ID
 deviceRoutes.get('/:deviceId/status', auth, DeviceController.getDeviceStatus);
+
 // PUT - turn a device ON/OFF
 deviceRoutes.put('/:deviceId/toggle', auth, DeviceController.toggleDeviceState);
 
