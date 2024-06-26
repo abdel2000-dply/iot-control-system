034631933d0aea6ffd49c0358378ef2db1f31eda
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-26 22:28:34 +0100

    Implement Device Routes

---
+++ routes/device.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import DeviceController from '../controllers/DeviceController';
+import auth from '../middleware/auth';
+
+const deviceRoutes = express.Router();
+
+// POST - Add a new device
+deviceRoutes.post('/', auth, DeviceController.newDevice);
+// GET - Get a device by ID
+deviceRoutes.get('/:deviceId', auth, DeviceController.getDeviceById);
+// GET - List all devices of that user
+deviceRoutes.get('', auth, DeviceController.listAllDevices);
+// GET - Get status of a device by ID
+deviceRoutes.get('/:deviceId/status', auth, DeviceController.getDeviceStatus);
+// PUT - turn a device ON/OFF
+deviceRoutes.put('/:deviceId/toggle', auth, DeviceController.toggleDeviceState);
+
+// PUT - Update a device by ID ==> Add this
+// DELETE - Delete a device by ID ==> Add this
+
+export default deviceRoutes;
