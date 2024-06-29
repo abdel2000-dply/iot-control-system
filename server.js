ecd5cf74f526df6757dc0af76d5b9edce149c566
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-29 18:58:14 +0100

    Update server.js to use socket.io for WebSocket communication

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import http from 'http';
+import { Server } from 'socket.io';
 import authRoutes from './routes/auth';
 import deviceRoutes from './routes/device';
+import { handleWebSocketConnection } from './websocket';
 
 dotenv.config();
 
@@ -10,6 +14,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
+
 app.use('/api/auth/', authRoutes);
 app.use('/api/devices', deviceRoutes); 
 
@@ -23,8 +29,17 @@ mongoose
   .then(() => console.log('MongoDB connected'))
   .catch((error) => console.log('MongoDB connection error:', error));
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', handleWebSocketConnection);
+
+server.listen(PORT, () => {
   console.log('Server is running on port', PORT);
 });
 
-export default app;
+export { app, server };
