3e0bfeb3383e567a037a8c304e88aa48fed24fd1
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-29 18:59:35 +0100

    Add WebSocket functionality for handling device data

---
+++ websocket.js
@@ -0,0 +1,26 @@
+import Device from './models/Device';
+
+const handleWebSocketConnection = (socket) => {
+  console.log('New connection:', socket.id);
+
+  socket.on('deviceData', async (data) => {
+    try {
+      const { deviceId, userId, deviceData } = data;
+      const device = await Device.findOne({ _id: deviceId, userId });
+      if (!device) {
+        return socket.emit('deviceError', { error: 'Device not found' });
+      }
+
+      device.lastSeen = Date.now();
+      device.status = 'online';
+      // handle the recived data
+
+      socket.emit('ack', 'Data received');
+    } catch (error) {
+      console.error(error);
+      socket.emit('deviceError', { error: error.message });
+    }
+  });
+};
+
+export { handleWebSocketConnection };
