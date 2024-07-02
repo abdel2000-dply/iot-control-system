5a813782981c902b36805fd33e42b75e9b107121
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-07-02 23:00:34 +0100

    Add logging for received device data

--- websocket.js
+++ websocket.js
@@ -1,23 +1,29 @@
 import Device from './models/Device';
+import logger from './utils/logger';
 
 const handleWebSocketConnection = (socket) => {
-  console.log('New connection:', socket.id);
+  logger.info(`New connection: ${socket.id}`);
 
   socket.on('deviceData', async (data) => {
     try {
       const { deviceId, userId, deviceData } = data;
       const device = await Device.findOne({ _id: deviceId, userId });
       if (!device) {
+        logger.error(`Device not found: ${deviceId}`);
         return socket.emit('deviceError', { error: 'Device not found' });
       }
 
       device.lastSeen = Date.now();
       device.status = 'online';
       // handle the recived data
-      console.log('Data received:', deviceData); // for now just log the data
+      logger.info(`Data received: ${deviceData}`); // for now just log the data
+
+      await device.save();
+
+      logger.info(`Data received from device: ${deviceId}`)
       socket.emit('ack', 'Data received');
     } catch (error) {
-      console.error(error);
+      logger.error(`Error receiving data: ${error.message}`);
       socket.emit('deviceError', { error: error.message });
     }
   });
