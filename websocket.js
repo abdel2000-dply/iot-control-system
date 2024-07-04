fbc7b8626199ad3834d63e5ffd8ada4296a37162
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-07-04 16:29:44 +0100

    Add authentication for WebSocket connections

--- websocket.js
+++ websocket.js
@@ -1,9 +1,41 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 import Device from './models/Device';
 import DeviceData from './models/DeviceData';
 import logger from './utils/logger';
 
+dotenv.config();
+
 const handleWebSocketConnection = (socket) => {
   logger.info(`New connection: ${socket.id}`);
+
+  socket.on('authenticate', async (token) => {
+    try {
+      const { userId, deviceId } = validateToken(token);
+      if (!userId || !deviceId) {
+        logger.error('Invalid token');
+        return socket.emit('authError', { error: 'Invalid token' });
+      }
+
+      const device = await Device.findOne({ _id: deviceId, userId });
+      if (!device) {
+        logger.error(`Device not found: ${deviceId}`);
+        return socket.emit('authError', { error: 'Device not found' });
+      }
+
+      // save the deviceid and userId in the socket for future use
+      socket.deviceId = deviceId;
+      socket.userId = userId;
+
+      // save the socket id in the device ???
+
+      socket.emit('authenticated', { message: 'Authenticated successfully' });
+      logger.info(`Device authenticated: ${deviceId}`);
+    } catch (error) {
+      logger.error(`Error authenticating device: ${error.message}`);
+      socket.emit('authError', { error: error.message });
+    }
+  });
 
   socket.on('deviceData', async (data) => {
     try {
@@ -14,6 +46,7 @@ const handleWebSocketConnection = (socket) => {
         - if not, how do we know which user is sending the data?
         - will the device send the deviceId?
         - if not, how do we know which device is sending the data?
+        ===== this would be solved by the added authentication for the device
       */
       const device = await Device.findOne({ _id: deviceId, userId });
       if (!device) {
