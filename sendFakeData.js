3f2ee305bde8a18621b85e0e51da8debb6b789fb
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-07-06 00:58:27 +0100

    Update sendFakeData.js to send simulated device data regularly

--- sendFakeData.js
+++ sendFakeData.js
@@ -1,20 +1,8 @@
 const io = require('socket.io-client');
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
 
-dotenv.config();
-
-// Replace these with appropriate values
 const serverUrl = 'http://localhost:5000';
 
-// JWT token got from registering a new device
-// const token =
-//   'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2Njg4MWViMmM3MDg0M2JlNGEwZTc1YTIiLCJkZXZpY2VJZCI6IjY2ODgxZjdjYzcwODQzYmU0YTBlNzVhNyIsImlhdCI6MTcyMDE5Njk4OH0.CnhToX6XXdKKMtn35L6zKyHkFGjGLuNSVEmaBqEi174';
-
-const userId = '66881eb2c70843be4a0e75a2';
-const deviceId = '66881f31c70843be4a0e75a5';
-
-const token = jwt.sign({ userId, deviceId }, process.env.JWT_SECRET);
+const token = '66881eb2c70843be4a0e75a2.6688845120e50024efb34f61';
 
 // Connect to the Socket.IO server
 const socket = io(serverUrl);
@@ -23,27 +11,27 @@ socket.on('connect', function () {
   console.log('Connected to the server');
 
   // Authenticate the device
-  socket.emit('authenticate', { token: `Bearer ${token}` });
+  socket.emit('authenticate', token);
 
   // Send fake data periodically
-  // setInterval(function () {
-  //   const fakeData = {
-  //     temperature: Math.random() * 30 + 10, // Random temperature between 10 and 40
-  //     humidity: Math.random() * 100 // Random humidity between 0 and 100
-  //   };
+  setInterval(function () {
+    const fakeData = {
+      temperature: Math.random() * 30 + 10, // Random temperature between 10 and 40
+      humidity: Math.random() * 100 // Random humidity between 0 and 100
+    };
 
-  //   socket.emit('deviceData', { deviceData: fakeData });
+    socket.emit('deviceData', { deviceData: fakeData });
 
-  //   console.log('Sent data:', fakeData);
-  // }, 5000); // Send data every 5 seconds
+    console.log('Sent data:', fakeData);
+  }, 10000); // Send data every 10 seconds
 
   // Send fake data once
-  socket.emit('deviceData', {
-    deviceData: {
-      temperature: 22,
-      humidity: 50
-    }
-  });
+  // socket.emit('deviceData', {
+  //   deviceData: {
+  //     temperature: 22,
+  //     humidity: 50
+  //   }
+  // });
 });
 
 socket.on('message', function (data) {
